a1e95864f1ef50460e60de6fff9894daed969a56
Luan Cordeiro <luam.lc19@gmail.com>
2023-03-13 21:12:36 -0300

    feat: create a chatItem
    there is a scroll-x bug

--- src/components/ChatItem/index.tsx
+++ src/components/ChatItem/index.tsx
@@ -1,9 +1,29 @@
-import { Container } from "./styles"
+import { ChatItemBody } from "./styles"
+
+type Props = {
+    img: string;
+    name: string;
+    date: string;
+    lastMessage: string;
+}
 
 export const ChatItem = () => {
     return (
-        <Container>
-            item
-        </Container>
+        <ChatItemBody>
+            <div className="chatItemBody--avatar">
+                <img src="https://graph.facebook.com/1584754295372323/picture" alt="" />
+            </div>
+            <div className="chatItemBody--lines">
+                <div className="chatItemBody--line">
+                    <div className="chatItemBody--name">Luan Cordeiro</div>
+                    <div className="chatItemBody--date">19:45</div>
+                </div>
+                <div className="chatItemBody--line">
+                    <div className="chatItemBody--lastMsg">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus voluptas quos vel beatae fugit, sunt ut labore numquam. Vitae cupiditate blanditiis quas autem laboriosam hic id placeat quidem aut quia!</p>
+                    </div>
+                </div>
+            </div>
+        </ChatItemBody>
     )
 }
--- src/components/ChatItem/styles.ts
+++ src/components/ChatItem/styles.ts
@@ -1,3 +1,59 @@
 import styled from "styled-components";
 
-export const Container = styled('div') ``;
+export const ChatItemBody = styled('div') `
+    height: 72px;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #DDD;
+    }
+
+    .chatItemBody--avatar img {
+        width: 50px;
+        height: 50px;
+        margin: 0 10px;
+        border-radius: 50%;
+    }
+
+    .chatItemBody--lines {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 10px 10px 10px 0;
+        height: 72px;
+        border-bottom: 1px solid #DDD;
+
+        .chatItemBody--line {
+            display: flex;
+            justify-content: space-between;
+
+            .chatItemBody--name {
+                font-weight: bold;
+            }
+
+            .chatItemBody--date {
+                font-size: 12px;
+                color: #999;
+            }
+
+            .chatItemBody--lastMsg {
+                font-size: 14px;
+                color: #999;
+                display: flex;
+                
+                flex-wrap: wrap;
+                width: 300px;
+
+                p {
+                    margin: 0;
+                    overflow: hidden;
+                    white-space: nowrap;
+                    text-overflow: ellipsis;
+                }
+            }
+        }
+    }
+`;
