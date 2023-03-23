a48feabdb340d13b7334995db89f6f05ad99646d
Luan Cordeiro <luam.lc19@gmail.com>
2023-03-23 20:33:58 -0300

    feat: create login screen

---
+++ src/components/Login/index.tsx
@@ -0,0 +1,25 @@
+import { User } from "firebase/auth";
+import { api } from "../../firebase"
+import { LoginBody } from "./styles"
+
+interface Props {
+    onRecive: (userInfo: User) => void;
+}
+
+export const Login = ({ onRecive }: Props) => {
+    const handleGoogleLogin = async () => {
+        let result = await api.gPopup();
+        if(result) {
+            console.log(result);
+            onRecive(result.user);
+        } else {
+            alert('error');
+        }
+    }
+
+    return (
+        <LoginBody>
+            <button onClick={handleGoogleLogin}>Logar com o google</button>
+        </LoginBody>
+    )
+}
